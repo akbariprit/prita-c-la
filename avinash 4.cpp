6be2e4df00c26b3b8306253cc457bc0116033c13
akbariprit <prit03566@gmail.com>
2025-05-21 17:04:49 +0530

    Add files via upload

---
+++ avinash 4.cpp
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+int main()
+{
+	int _noob = 69;
+	
+	float _pro = 23;
+	
+	char _hacker = 'a';
+	
+	printf("%d\n",_noob);
+	printf("%f\n",_pro);
+	printf("value of hacker is : %c",_hacker); 
+}
