22f285f0a0a1bab3cd7e4655f259e72fc3f17181
akbariprit <prit03566@gmail.com>
2025-05-22 18:28:36 +0530

    Add files via upload

---
+++ prit 1.2.1.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	printf("akbari prit\n");
+	printf(" 17\n");
+	printf("taravada gurukul");
+}
---
+++ prit 2.1.2.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+	int a,b,c;
+	a=5;
+	b=5;
+	c= a*b;
+	     printf("--------------\n");
+	     printf("|            |\n");
+	     printf("| 5 * 5 = %d |\n",c);
+	     printf("|            |\n");
+	     printf("--------------\n");
+}
